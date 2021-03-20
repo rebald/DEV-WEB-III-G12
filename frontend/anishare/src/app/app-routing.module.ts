f6d17a38c04dcb4c9db805ba49840162e7c13f94
rebald <43856164+rebald@users.noreply.github.com>
2021-03-21 00:20:53 +0100

    contact + home page

---
+++ frontend/anishare/src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { Component, NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ContactComponent } from './contact/contact.component';
+
+const routes: Routes = [
+  {
+    path:'',
+    component: Component
+  },
+  {
+    path:'contact',
+    component : ContactComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
