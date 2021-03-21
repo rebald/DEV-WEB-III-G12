e1b407e3b9cc83ac776c161380e05da5eab72638
rebald <43856164+rebald@users.noreply.github.com>
2021-03-21 02:24:00 +0100

    about page

--- frontend/anishare/src/app/app-routing.module.ts
+++ frontend/anishare/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ContactComponent } from './contact/contact.component';
 import { SearchComponent } from './search/search.component';
+import { PropoComponent } from './propo/propo.component';
 
 const routes: Routes = [
   {
@@ -11,6 +12,10 @@ const routes: Routes = [
   {
     path:'contact',
     component : ContactComponent
+  },
+  {
+    path:'apropos',
+    component : PropoComponent
   }
 ];
 
