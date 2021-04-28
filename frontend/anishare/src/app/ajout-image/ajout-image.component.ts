1e0e71e29d4927afadc980995a79c8eacbb0897e
Séverin Robert <sevrobe21@gmail.com>
2021-04-28 11:57:24 +0200

    ajout-image component

---
+++ frontend/anishare/src/app/ajout-image/ajout-image.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ajout-image',
+  templateUrl: './ajout-image.component.html',
+  styleUrls: ['./ajout-image.component.scss']
+})
+export class AjoutImageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
