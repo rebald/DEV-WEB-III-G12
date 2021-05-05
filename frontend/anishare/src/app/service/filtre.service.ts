f136eae2cbc5cca8f2e3b34f262f1f801df793ff
maxime <maxime.de.cock98@gmail.com>
2021-05-05 15:42:00 +0200

    environement liaison api

--- frontend/anishare/src/app/service/filtre.service.ts
+++ frontend/anishare/src/app/service/filtre.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import {Observable} from 'rxjs';
+// import {environment} from '../../environments/environment';
 
 @Injectable({providedIn: 'root'})
 export class FiltreService
@@ -11,3 +12,4 @@ export class FiltreService
     }
 }
 
+//  return this.http.get(environment.apiUrl + '/categorie');
